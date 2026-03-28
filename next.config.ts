22a28be1dde22e19c17aa15f9d509e4ee3928f92
Hamxa_500 <32429165+hamza500@users.noreply.github.com>
2026-03-28 12:15:21 +0500

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    reactCompiler: true,
-  },
-  suppressHydrationWarning: false,
+  /* config options here */
 };
 
 export default nextConfig;
